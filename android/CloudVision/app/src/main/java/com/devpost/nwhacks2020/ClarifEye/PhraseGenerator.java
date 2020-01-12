c8af49846a5bd1cb669b1dff1b2b7721eacead78
Justin van Heek <justin.vanheek@vhsoftware.dev>
2020-01-11 18:27:34 -0800

    Functioning TTS

--- android/CloudVision/app/src/main/java/com/devpost/nwhacks2020/ClarifEye/PhraseGenerator.java
+++ android/CloudVision/app/src/main/java/com/devpost/nwhacks2020/ClarifEye/PhraseGenerator.java
@@ -1,4 +1,8 @@
 package com.devpost.nwhacks2020.ClarifEye;
 
 public class PhraseGenerator {
+
+    public static String generatePhrase() {
+        return "This is a test of the Clarify app.";
+    }
 }
