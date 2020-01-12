f8e9a6c4714c342ce771e542a1365514550d78a0
Sam Delaney <sam@thedelaneys.net>
2020-01-11 18:10:59 -0800

    Add List of Prepositions to Phrase Generator

--- android/CloudVision/app/src/main/java/com/devpost/nwhacks2020/ClarifEye/PhraseGenerator.java
+++ android/CloudVision/app/src/main/java/com/devpost/nwhacks2020/ClarifEye/PhraseGenerator.java
@@ -1,4 +1,13 @@
 package com.devpost.nwhacks2020.ClarifEye;
 
 public class PhraseGenerator {
+    private String[] prepPhrases = {
+        " and ", //generic
+        " above ",
+        " below ",
+        " beside ",
+        " in front of ", //distinguish from below
+        " behind "       //distinguish from above
+    };
 }
+
