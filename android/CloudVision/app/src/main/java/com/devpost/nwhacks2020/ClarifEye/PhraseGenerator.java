7370d44433b78a534dcdd30ba77aaeba07a6d809
Sam Delaney <sam@thedelaneys.net>
2020-01-11 20:31:20 -0800

    Add pseudocode for generatePhraseDescribe and dependent methods/objects

--- android/CloudVision/app/src/main/java/com/devpost/nwhacks2020/ClarifEye/PhraseGenerator.java
+++ android/CloudVision/app/src/main/java/com/devpost/nwhacks2020/ClarifEye/PhraseGenerator.java
@@ -3,13 +3,14 @@ package com.devpost.nwhacks2020.ClarifEye;
 import com.google.api.services.vision.v1.model.AnnotateImageResponse;
 
 public class PhraseGenerator {
-    private String[] prepPhrases = {
-        " and ", //generic
-        " above ",
-        " below ",
-        " beside ",
-        " in front of ", //distinguish from below
-        " behind "       //distinguish from above
+    private static String[] prepPhrases = {
+            " and ", //generic
+            " above ",
+            " below ",
+            " beside ",
+            " with ",        //affects/disregards sentence emphasis
+            " in front of ", //distinguish from below
+            " behind "       //distinguish from above
     };
 
     public static String generatePhrase(AnnotateImageResponse annotateImageResponse, VisionRequestor.Mode mode) {
@@ -25,11 +26,49 @@ public class PhraseGenerator {
         return phrase;
     }
 
-    public static String generatePhraseDescribe(AnnotateImageResponse annotateImageResponse) {
+    private static String generatePhraseDescribe(AnnotateImageResponse annotateImageResponse) {
+        //objects = create_objects (annotateImageResponse)
+
+        //objects = array.sort(objects)
+
+        //ppairs = build_preposition_pairs (objects)
+
+        // return build_string(objects, ppairs)
+        return "This is a test of the Clarify app.";
+    }
+
+    private static String generatePhraseRead(AnnotateImageResponse annotateImageResponse) {
         return "";
     }
-    
-    public static String generatePhraseRead(AnnotateImageResponse annotateImageResponse) {
-        return "";
+
+    private class item{
+        private item() {
+            //calculate and store importance
+            //overload comparison
+        }
     }
+
+    private class prepositionPair{
+        private prepositionPair() {
+            int prepositionIndex;
+            int adjunctIndex;
+            int subjectIndex; //rename variable
+            double score;
+
+            //overload comparison
+        }
+    }
+
+    // create_objects (AnnotateImageResponse > item[])
+
+    // build_preposition_pairs ( item[] ) > prepositionPair[]
+        //foreach i in item.length
+            //compare(item[i], item[i+1])
+
+    //compare(item1, item2) > prepositionPair
+
+    //build_string(item[], prepositionPair[])
+        //foreach(item[])
+            //get best prepositionPair with item[i]
 }
+
